b30c9805f8b91c4ea0214a508e250d75c50da627
grizzledwarvet23 <72232563+grizzledwarvet23@users.noreply.github.com>
2020-10-14 17:39:24 -0500

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,37 @@
 package frc.robot.subsystems;
 
+
 public class Intake {
+    XboxController controller;
+    VictorSPX intakeMotor;
+    
+    /** Constructs the intake motor along with 
+     *  the Xbox Controller
+     *
+     */
+    public Intake(XboxController x) {
+        controller = x;
+        intakeMotor = new VictorSPX(Constants.INTAKE_MOTOR_ID);
+    }
+
+    public void teleOp()    
+    {
+        //Intake
+        if(controller.buttonX())
+        intakeMotor.set(1);
+        else if(controller .buttonY()) {
+                 intakeMotor.set (-1);
+        }
+        else {
+                intakeMotor.set (0);
+            //#endregion
+        }
+        
+
+
+    }
+
+
+
 
 }
