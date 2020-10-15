092b770c042ffb109aeb5d5a9a073494b0bc22df
grizzledwarvet23 <72232563+grizzledwarvet23@users.noreply.github.com>
2020-10-15 17:18:11 -0500

    Filled the ControlPanel class
    Constructor, basic teleop logic, javadocs

--- src/main/java/frc/robot/subsystems/ControlPanel.java
+++ src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -1,17 +1,36 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.Constants;
+
 /**
- * A one motor system that can move in either direction. 
+ * A one motor system that can move in either direction.
  */
 public class ControlPanel {
+    
+    private XboxController controller;
+    private PWMVictorSPX panelMotor;
 
-    public ControlPanel() 
+    /**
+     * Constructs a ControlPanel object.
+     * @param x the XboxController
+     */
+    public ControlPanel(XboxController x)
     {
-
+        controller = x;
+        panelMotor = new PWMVictorSPX(Constants.CONTROL_MOTOR_ID);
     }
 
-    public void teleOp() 
-    {
-        
+    public void teleOp()
+    {        
+        //#region ControlPanel
+        if(controller.getAButton() && !controller.getBButton()) 
+            panelMotor.set(1);
+        else if(controller.getBButton() && !controller.getAButton())
+            panelMotor.set(-1);
+        else
+            panelMotor.set(0);
+        //#endregion
     }
 }
