419d168dccde5fb3d095a5e6e63c418d0e3204ed
Joshua Purushothaman <52509691+joshuathooyavan@users.noreply.github.com>
2020-10-19 17:30:20 -0500

    Control Panel - added some Color Sensor code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.I2C.Port;
+
 public class Constants
 {
     public final static int LF_MOTOR_ID = 1;
@@ -12,4 +14,6 @@ public class Constants
     public final static int INTAKE_MOTOR_ID = 7;
     public final static int LIFT_MOTOR_ID = 8;
     public final static int SHOOTER_MOTOR_ID = 9;
+    
+	public final static Port COLOR_SENSOR_ID = Port.kOnboard;
 }
--- src/main/java/frc/robot/subsystems/ControlPanel.java
+++ src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -1,7 +1,15 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+import edu.wpi.first.wpilibj.util.Color;
+
 import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.XboxController;
+
+import edu.wpi.first.wpilibj.DriverStation;
+
 import frc.robot.Constants;
 
 /**
@@ -12,6 +20,15 @@ public class ControlPanel {
     private XboxController controller;
     private PWMVictorSPX panelMotor;
 
+    private ColorSensorV3 colorSensor;
+    private final ColorMatch colorMatcher = new ColorMatch();
+
+    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+    
+
     /**
      * Constructs a ControlPanel object.
      * @param x the XboxController
@@ -20,17 +37,54 @@ public class ControlPanel {
     {
         controller = x;
         panelMotor = new PWMVictorSPX(Constants.CONTROL_MOTOR_ID);
+        colorSensor = new ColorSensorV3(Constants.COLOR_SENSOR_ID);
+        
+        colorMatcher.addColorMatch(kBlueTarget);
+        colorMatcher.addColorMatch(kGreenTarget);
+        colorMatcher.addColorMatch(kRedTarget);
+        colorMatcher.addColorMatch(kYellowTarget);
     }
-
+    
     public void teleOp()
-    {        
-        //#region ControlPanel
-        if(controller.getAButton() && !controller.getBButton()) 
-            panelMotor.set(1);
-        else if(controller.getBButton() && !controller.getAButton())
-            panelMotor.set(-1);
-        else
-            panelMotor.set(0);
-        //#endregion
+    { 
+        // //#region ControlPanel
+        // if(controller.getAButton() && !controller.getBButton()) 
+        //     panelMotor.set(1);
+        // else if(controller.getBButton() && !controller.getAButton())
+        //     panelMotor.set(-1);
+        // else
+        //     panelMotor.set(0);
+        // //#endregion
+    }
+    /**
+     * Gets the color of this match
+     */
+    private void getGameColor()
+    {
+        String gameData;
+        gameData = DriverStation.getInstance().getGameSpecificMessage();
+        if(gameData.length() > 0)
+        {
+            switch (gameData.charAt(0))
+            {
+                case 'B' :
+                    //Blue case code
+                    break;
+                case 'G' :
+                    //Green case code
+                    break;
+                case 'R' :
+                    //Red case code
+                    break;
+                case 'Y' :
+                    //Yellow case code
+                    break;
+                default :
+                    //This is corrupt data
+                    break;
+            }
+        } else {
+            //Code for no data received yet
+        }
     }
 }
