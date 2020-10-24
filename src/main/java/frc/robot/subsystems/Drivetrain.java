f6f99405cc0d0183e988d4cc3e51432a0e3eca7a
Joshua Purushothaman <52509691+joshuathooyavan@users.noreply.github.com>
2020-10-24 05:56:01 -0500

    Cleanup, changed to CANSparkMax

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,8 +1,10 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -10,130 +12,99 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import frc.robot.Constants;
 
-import frc.robot.*;
-
+/**
+ * Based off of DriveSubsystem example in RamseteCommand
+ */
 public class Drivetrain
 {
-  // The motors on the left side of the drive.
-  private final SpeedControllerGroup m_leftMotors =
-      new SpeedControllerGroup(new PWMVictorSPX(Constants.LF_MOTOR_ID),
-                               new PWMVictorSPX(Constants.LB_MOTOR_ID));
-
-  // The motors on the right side of the drive.
-  private final SpeedControllerGroup m_rightMotors =
-      new SpeedControllerGroup(new PWMVictorSPX(Constants.RF_MOTOR_ID),
-                               new PWMVictorSPX(Constants.RB_MOTOR_ID));
-
-  // The robot's drive
-  private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
-
-  // The left-side drive encoder
-  private final Encoder m_leftEncoder =
-      new Encoder(Constants.kLeftEncoderPorts[0], Constants.kLeftEncoderPorts[1],
-                  Constants.kLeftEncoderReversed);
-
-  // The right-side drive encoder
-  private final Encoder m_rightEncoder =
-      new Encoder(Constants.kRightEncoderPorts[0], Constants.kRightEncoderPorts[1],
-                  Constants.kRightEncoderReversed);
-
-  // The gyro sensor
-  public final Gyro m_gyro = new ADXRS450_Gyro();
-
-  // Odometry class for tracking robot pose
-  private final DifferentialDriveOdometry m_odometry;
-
-  PIDController leftPID = new PIDController(1, 0, 0);   //  https://frc-pdr.readthedocs.io/en/latest/control/pid_control.html#tuning-methods oooh!
-  PIDController rightPID = new PIDController(1, 0, 0);
-
-  /**
-   * Creates a new DriveSubsystem.
-   */
-  public Drivetrain() {
-    m_leftEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
-    m_rightEncoder.setDistancePerPulse(Constants.kEncoderDistancePerPulse);
-
-    m_leftEncoder.reset();
-    m_rightEncoder.reset();
+    CANSparkMax lf = new CANSparkMax(Constants.LF_MOTOR_ID, MotorType.kBrushless);
+    CANSparkMax rf = new CANSparkMax(Constants.RF_MOTOR_ID, MotorType.kBrushless);
     
-    m_odometry = new DifferentialDriveOdometry(new Rotation2d(Math.toRadians(m_gyro.getAngle())));
-  }
+    CANEncoder m_leftEncoder = new CANEncoder(lf);
+    CANEncoder m_rightEncoder = new CANEncoder(rf);
 
-  /**
-   * call me during periodic pls ;)
-   */
-  public void updateOdometry()
-  {
+    private final SpeedControllerGroup m_leftMotors =
+        new SpeedControllerGroup(lf, new CANSparkMax(Constants.LB_MOTOR_ID, MotorType.kBrushless));
+
+    private final SpeedControllerGroup m_rightMotors =
+        new SpeedControllerGroup(rf, new CANSparkMax(Constants.RB_MOTOR_ID, MotorType.kBrushless));                               
+
+    private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
+    
+    public final Gyro m_gyro = new ADXRS450_Gyro();
+
+    // Odometry class for tracking robot pose
+    private final DifferentialDriveOdometry m_odometry;
+
+    PIDController leftPID = new PIDController(1, 0, 0);   //  https://frc-pdr.readthedocs.io/en/latest/control/pid_control.html#tuning-methods oooh!
+    PIDController rightPID = new PIDController(1, 0, 0);
+
+    /**
+     * Creates a new DriveSubsystem.
+     */
+    public Drivetrain() {
+        m_leftEncoder.setPositionConversionFactor(Constants.kEncoderDistancePerPulse);
+        m_rightEncoder.setPositionConversionFactor(Constants.kEncoderDistancePerPulse);
+
+        m_leftEncoder.setVelocityConversionFactor(Constants.kEncoderDistancePerPulse);
+        m_rightEncoder.setVelocityConversionFactor(Constants.kEncoderDistancePerPulse);
+
+        m_odometry = new DifferentialDriveOdometry(new Rotation2d(Math.toRadians(m_gyro.getAngle())));
+    }
+
+    /**
+     * call me during periodic pls ;)
+     */
+    public void updateOdometry()
+    {
     // Update the odometry in the periodic block
-    m_odometry.update(new Rotation2d(Math.toDegrees(m_gyro.getAngle())), m_leftEncoder.getDistance(),
-                      m_rightEncoder.getDistance());
-  }
+    m_odometry.update(new Rotation2d(Math.toDegrees(m_gyro.getAngle())), m_leftEncoder.getPosition(),  //  need to make sure getPosition is same as getDistance
+                        m_rightEncoder.getPosition());
+    }
 
-  /**
-   * Returns the currently-estimated pose of the robot.
-   *
-   * @return The pose.
-   */
-  public Pose2d getPose() {
-    return m_odometry.getPoseMeters();
-  }
+    /**
+     * Returns the currently-estimated pose of the robot.
+     *
+     * @return The pose.
+     */
+    public Pose2d getPose() {
+        return m_odometry.getPoseMeters();
+    }
 
-  /**
-   * Returns the current wheel speeds of the robot.
-   *
-   * @return The current wheel speeds.
-   */
-  public DifferentialDriveWheelSpeeds getWheelSpeeds() {
-    return new DifferentialDriveWheelSpeeds(m_leftEncoder.getRate(), m_rightEncoder.getRate());
-  }
+    /**
+     * Drives the robot using arcade controls.
+     *
+     * @param fwd the commanded forward movement
+     * @param rot the commanded rotation
+     */
+    public void arcadeDrive(double fwd, double rot) {
+        m_drive.arcadeDrive(fwd, rot);
+    }
 
-  /**
-   * Drives the robot using arcade controls.
-   *
-   * @param fwd the commanded forward movement
-   * @param rot the commanded rotation
-   */
-  public void arcadeDrive(double fwd, double rot) {
-    m_drive.arcadeDrive(fwd, rot);
-  }
+    /**
+     * Controls the left and right sides of the drive directly with voltages.
+     *
+     * @param leftVolts  the commanded left output
+     * @param rightVolts the commanded right output
+     */
+    public void tankDriveVolts(double leftVolts, double rightVolts) {
+        m_leftMotors.setVoltage(leftVolts);
+        m_rightMotors.setVoltage(-rightVolts);
+        m_drive.feed();
+    }
 
-  /**
-   * Controls the left and right sides of the drive directly with voltages.
-   *
-   * @param leftVolts  the commanded left output
-   * @param rightVolts the commanded right output
-   */
-  public void tankDriveVolts(double leftVolts, double rightVolts) {
-    m_leftMotors.setVoltage(leftVolts);
-    m_rightMotors.setVoltage(-rightVolts);
-    m_drive.feed();
-  }
-
-  public void driveBySpeeds(double leftSpeed, double rightSpeed)
-  {
+    /**
+     * Sets the drivetrain to a given speed.
+     * @param leftSpeed speed of left side
+     * @param rightSpeed speed of right side, duh lmao
+     */
+    public void driveBySpeeds(double leftSpeed, double rightSpeed)
+    {
     this.tankDriveVolts(
-        leftPID.calculate(m_leftEncoder.getRate(), leftSpeed), 
-        rightPID.calculate(m_rightEncoder.getRate(), rightSpeed)
+        leftPID.calculate(m_leftEncoder.getVelocity(), leftSpeed), 
+        rightPID.calculate(m_rightEncoder.getVelocity(), rightSpeed)
         );  //  Maybe .getDistance works if this doesn't??? Test and find out
-  }
-
-  /**
-   * Sets the max output of the drive.  Useful for scaling the drive to drive more slowly.
-   *
-   * @param maxOutput the maximum output to which the drive will be constrained
-   */
-  public void setMaxOutput(double maxOutput) {
-    m_drive.setMaxOutput(maxOutput);
-  }
-
-  /**
-   * Returns the heading of the robot.
-   *
-   * @return the robot's heading in degrees, from -180 to 180
-   */
-  public double getHeading() {
-    return m_gyro.getAngle()-180;
-  }
+    }
 }
